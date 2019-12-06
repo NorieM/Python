2882ba68bd67aa01d08eeb9dc4e8d39ebae51bb5
Norie MacEwan <50264139+NorieM@users.noreply.github.com>
2019-12-06 12:40:15 +0000

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 
 from flask import render_template
 from app import app
+from app.forms import LogInForm
+
 
 @app.route('/')
-@app.route('/index')
 
+@app.route('/index')
 def index():
     user = {'username':'Norie'}
 
@@ -19,3 +21,8 @@ def index():
     ]
 
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LogInForm()
+    return render_template('login.html', title='Sign In', form=form)
